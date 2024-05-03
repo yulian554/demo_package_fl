326903a211e1a29bc37454e66fc1387ed2d7a32c
yulianhincapie <yulian.hincapie@pragma.com.co>
2024-05-03 10:59:42 -0500

    test

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,11 +1,7 @@
 library package_test;
 
-import 'package:package_test/domain/use_case/get_pokemons_use_case.dart';
-import 'package:package_test/domain/use_case/impl/get_pokemons_use_case_impl.dart';
-
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
-  final GetPokemonsUseCase useCase = GetPokemonsUseCaseImpl();
 }
